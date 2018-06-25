f76ac67ef352582606a2fd544d3be251b1c2316c
Etienne Pauthenet <etienne.pauthenet@gmail.com>
2018-06-25 11:36:38 +0200

    Add fda.oce

---
+++ R/fda.oce.R
@@ -0,0 +1,10 @@
+#' fda.oce: A package for the use of functional data analysis tools on oceanographic profiles.
+#'
+#' The fda.oce package provides two categories of functions : computation :
+#' \code{bspl}, \code{fpca}, \code{reco}, \code{proj}
+#' and visualisation :
+#' \code{eigenf},\code{map_pc},\code{kde_pc}.
+#'
+#' @docType package
+#' @name fda.oce
+NULL
