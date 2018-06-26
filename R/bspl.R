3e9a794c321d92d85e511c11eae08348e51ab074
Etienne Pauthenet <etienne.pauthenet@gmail.com>
2018-06-26 09:57:46 +0200

    add vertical irregular sampling case

--- R/bspl.R
+++ R/bspl.R
@@ -8,8 +8,9 @@
 #' @param range vector of two elements containing the first and last depth level \code{c(dmin,dmax)}, \code{dmin} and \code{dmax} must be contained by the \code{depth} range.
 #' @param mybn number of splines, Default is mybn = 20.
 #' @param l number of interpolation point. The number of points to interpolate must be large enough to avoid overfitting. Default is l = 100.
-#' @param interp is TRUE if the data provided is already interpolated on a thin vertical grid (1 dbar by level). For example \code{DATA_ncARGO_interp} from MEOP.
+#' @param irreg default is 0 for irregular vertical sampling and regular horizontal sampling (model type e.g. SOSE, GLORYS,...). \code{irreg=1} for irregular vertical and horizontal sampling (e.g. \code{DATA_ncARGO} for MEOP). \code{irreg=2} for irregular horizontal sampling but interpolated on a thin vertical grid (1 dbar by level).(e.g. \code{DATA_ncARGO_interp} from MEOP).
 
+#'
 #' @return \code{temp.fd} and \code{sal.fd} : fd objects of the splines construction containing coefficients, etc...
 #' @return  \code{BS} list containing :\code{profx}, \code{Teval} and \code{Seval} (the splines sampled along profx)
 #' @return  \code{mask} : if the data was on a regular horizontal grid
@@ -18,13 +19,13 @@
 #' @seealso \code{\link{fpca}} for functional principal component analysis of T-S profiles, \code{\link{PCmap}} for plotting a map of PC, \code{\link{kde_pc}} for kernel density estimation of two PCs...
 
 #' @export
-bspl <- function(temp,sal,Depth,range,mybn = 20,l = 100,interp = FALSE){
+bspl <- function(temp,sal,Depth,range,mybn = 20,l = 100,irreg = 0){
   cat("Converting the profiles into Bsplines...")
   dmin <- range[1]
   dmax <- range[2]
   profx <- seq(dmin,dmax,length = l)
 
-  if(interp == FALSE){
+  if(irreg == 0){
     if (length(dim(temp))==2){
       Tm <- t(temp)
       Sm <- t(sal)
@@ -88,6 +89,7 @@ bspl <- function(temp,sal,Depth,range,mybn = 20,l = 100,interp = FALSE){
     Tfd <- fda::eval.fd(profx,temp.fd)
     Sfd <- fda::eval.fd(profx,sal.fd)
 
+    #Creation of a mask of the map, when applicable
     if (length(dim(temp))==3){
       mask = matrix(NaN,n,m)
       mask[ind] = 1
@@ -97,6 +99,7 @@ bspl <- function(temp,sal,Depth,range,mybn = 20,l = 100,interp = FALSE){
       mask = mask[,,1]
     }
 
+    #return
     BS        <<- list(ind,profx,Tfd,Sfd)
     names(BS) <<- c('ind','profx','Teval','Seval')
     temp.fd   <<- temp.fd
@@ -104,7 +107,43 @@ bspl <- function(temp,sal,Depth,range,mybn = 20,l = 100,interp = FALSE){
     mask      <<- mask
     cat(paste(length(BS$ind)," profiles converted in Bsplines."))
 
-  }else if (interp == TRUE){         ##############For already interpolated data on a thin grid
+  }else if (irreg == 1){         ##############For irregular horizontal and vertical sampling
+    k     <- 1
+    t     <- temp[,k]
+    s     <- sal[,k]
+    profx <- Depth[dmin:dmax]
+
+    # Calcule les splines du profil 1
+    myb     <- fda::create.bspline.basis(c(dmin,dmax),mybn)
+    templ   <- approx(depth,t,method = 'linear',xout = profx)$y
+    temp.fd <- fda::Data2fd(profx,templ,myb)
+    sall    <- approx(depth,s,method = 'linear',xout = profx)$y
+    sal.fd  <- fda::Data2fd(profx,sall,myb)
+    ind     <- k
+
+    for (k in which(is.na(temp[dmax,])==FALSE)[2]:dim(temp)[2]){  #In case of NaN in the matrix
+      t <- temp[,k]
+      s <- sal[,k]
+      templ         <- approx(depth,t,method = 'linear',xout = profx)$y
+      tmpt.fd       <- fda::Data2fd(profx,templ,myb)
+      sall          <- approx(depth,s,method = 'linear',xout = profx)$y
+      tmps.fd       <- fda::Data2fd(profx,sall,myb)
+      temp.fd$coefs <- cbind(temp.fd$coefs,tmpt.fd$coefs)
+      sal.fd$coefs  <- cbind(sal.fd$coefs,tmps.fd$coefs)
+      ind <- c(ind,k)
+    }
+
+    Tfd <- fda::eval.fd(profx,temp.fd)
+    Sfd <- fda::eval.fd(profx,sal.fd)
+
+    #Return
+    BS        <<- list(ind,profx,Tfd,Sfd)
+    names(BS) <<- c('ind','profx','Teval','Seval')
+    temp.fd   <<- temp.fd
+    sal.fd    <<- sal.fd
+    cat(paste(length(BS$ind)," profiles converted in Bsplines"))
+
+  }else if (irreg == 2){         ##############For irregular horizontal sampling and interpolated data on a thin grid
     k     <- 1
     t     <- temp[,k]
     s     <- sal[,k]
